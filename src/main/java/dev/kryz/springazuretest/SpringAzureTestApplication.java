890040da6c5b88d68e46cf12a79bc04cceec80bb
Krzysztof Piechowicz <krzysztof.piechowicz@adesso.de>
2023-05-20 20:22:52 +0200

    initial

---
+++ src/main/java/dev/kryz/springazuretest/SpringAzureTestApplication.java
@@ -0,0 +1,13 @@
+package dev.kryz.springazuretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAzureTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/kryz/springazuretest/SpringAzureTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kryz.springazuretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
